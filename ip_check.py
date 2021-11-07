5700eaf5e9a6cb64818930e11e69f525c1f28a70
svds90 <svds22090@gmail.com>
2021-11-07 19:26:10 +0300

    ip_check edit

--- ip_check.py
+++ ip_check.py
@@ -1,19 +1,27 @@
-# -*- coding: utf-8 -*-
-
-ip = input('Введите ip-адрес в формате X.X.X.X: ')
+ip = input('Enter IP in X.X.X.X format: ')
 octets = ip.split('.')
-octets_int = []
+octets_correct = True
 
-for octet in octets:
-    octets_int.append(int(octet))
-
-if octets_int[0] > 0 and octets_int[0] <= 223:
-    print('unicast')
-elif octets_int[0] >= 224 and octets_int[0] <= 239:
-    print('multicast')
-elif ip == '255.255.255.255':
-    print('local broadcast')
-elif ip == '0.0.0.0':
-    print('unassigned')
+if len(octets) != 4:
+    octets_correct = False
 else:
-    print('unused')
+    for octet in octets:
+        if not(octet.isdigit() and int(octet) in range(256)):
+            octets_correct = False
+            break
+
+if not octets_correct:
+    print('\n Wrong IP\n')
+else:
+    octets_int = [int(oct) for oct in octets]
+
+    if octets_int[0] > 0 and octets_int[0] <= 223:
+        print('unicast')
+    elif octets_int[0] >= 224 and octets_int[0] <= 239:
+        print('multicast')
+    elif ip == '255.255.255.255':
+        print('local broadcast')
+    elif ip == '0.0.0.0':
+        print('unassigned')
+    else:
+        print('unused')
